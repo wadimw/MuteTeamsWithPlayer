38e9dd70ee8e0548fda61ec245efd9d6d5aa5272
wadimw <wadim11@gmail.com>
2021-08-16 11:12:10 +0200

    Initial Commit

---
+++ MuteTeamsWithPlayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MuteTeamsWithPlayer
+//
+//  Created by Wadim on 16/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MuteTeamsWithPlayer/MuteTeamsWithPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuteTeamsWithPlayerApp.swift
+//  MuteTeamsWithPlayer
+//
+//  Created by Wadim on 16/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct MuteTeamsWithPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
